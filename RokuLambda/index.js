45d725b7a1d6bc2660c42c281438fb557b91d829
Julian Hartline <julianh2o@gmail.com>
2016-12-27 11:22:36 -0800

    replacing tabs with spaces in index.js

--- RokuLambda/index.js
+++ RokuLambda/index.js
@@ -18,271 +18,271 @@ AlexaRoku.prototype.constructor = AlexaRoku;
 function sendCommand(path,body,callback) {
     var opt = {
         host:serverinfo.host,
-		port:serverinfo.port,
+        port:serverinfo.port,
         path: path,
         method: 'POST',
     };
 
     var req = http.request(opt, function(res) {
-		callback();
+        callback();
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             console.log('Response: ' + chunk);
         });
     });
 
-	if (body) req.write(body);
+    if (body) req.write(body);
     req.end();
 }
 
 AlexaRoku.prototype.intentHandlers = {
     Home: function (intent, session, response) {
-		sendCommand("/roku/home",null,function() {
-			response.tellWithCard("Going Home");
-		});
+        sendCommand("/roku/home",null,function() {
+            response.tellWithCard("Going Home");
+        });
     },
     Amazon: function (intent, session, response) {
-		sendCommand("/roku/amazon",null,function() {
-			response.tellWithCard("Launching Amazon");
-		});
+        sendCommand("/roku/amazon",null,function() {
+            response.tellWithCard("Launching Amazon");
+        });
     },
     Pandora: function (intent, session, response) {
-		sendCommand("/roku/pandora",null,function() {
-			response.tellWithCard("Launching Pandora");
-		});
+        sendCommand("/roku/pandora",null,function() {
+            response.tellWithCard("Launching Pandora");
+        });
     },
     Hulu: function (intent, session, response) {
-		sendCommand("/roku/hulu",null,function() {
-			response.tellWithCard("Launching Hulu");
-		});
+        sendCommand("/roku/hulu",null,function() {
+            response.tellWithCard("Launching Hulu");
+        });
     },
     Captionson: function (intent, session, response) {
-		sendCommand("/roku/captionson",null,function() {
-			response.tellWithCard("Turning On Captions");
-		});
+        sendCommand("/roku/captionson",null,function() {
+            response.tellWithCard("Turning On Captions");
+        });
     },
     Captionsoff: function (intent, session, response) {
-		sendCommand("/roku/captionsoff",null,function() {
-			response.tellWithCard("Turning Off Captions");
-		});
+        sendCommand("/roku/captionsoff",null,function() {
+            response.tellWithCard("Turning Off Captions");
+        });
     },
     Select: function (intent, session, response) {
-		sendCommand("/roku/select",null,function() {
-			response.tellWithCard("Ok");
-		});
+        sendCommand("/roku/select",null,function() {
+            response.tellWithCard("Ok");
+        });
     },
     Back: function (intent, session, response) {
-		sendCommand("/roku/back",null,function() {
-			response.tellWithCard("Going Back");
-		});
+        sendCommand("/roku/back",null,function() {
+            response.tellWithCard("Going Back");
+        });
     },
     TV: function (intent, session, response) {
-		sendCommand("/roku/tv",null,function() {
-			response.tellWithCard("TV");
-		});
+        sendCommand("/roku/tv",null,function() {
+            response.tellWithCard("TV");
+        });
     },
     Plex: function (intent, session, response) {
-		sendCommand("/roku/plex",null,function() {
-			response.tellWithCard("Launching Plex");
-		});
+        sendCommand("/roku/plex",null,function() {
+            response.tellWithCard("Launching Plex");
+        });
     },
     HBO: function (intent, session, response) {
-		sendCommand("/roku/hbo",null,function() {
-			response.tellWithCard("Launching HBO");
-		});
+        sendCommand("/roku/hbo",null,function() {
+            response.tellWithCard("Launching HBO");
+        });
     },
     YouTube: function (intent, session, response) {
-		sendCommand("/roku/youtube",null,function() {
-			response.tellWithCard("Launching YouTube");
-		});
-	
+        sendCommand("/roku/youtube",null,function() {
+            response.tellWithCard("Launching YouTube");
+        });
+    
     },
     FX: function (intent, session, response) {
-		sendCommand("/roku/fx",null,function() {
-			response.tellWithCard("Launching FX");
-		});
-	
+        sendCommand("/roku/fx",null,function() {
+            response.tellWithCard("Launching FX");
+        });
+    
     },
     FourK: function (intent, session, response) {
-		sendCommand("/roku/fourk",null,function() {
-			response.tellWithCard("Launching Four K");
-		});
-	
+        sendCommand("/roku/fourk",null,function() {
+            response.tellWithCard("Launching Four K");
+        });
+    
     },
     Rewind: function (intent, session, response) {
-		sendCommand("/roku/rewind",null,function() {
-			response.tellWithCard("Rewinding");
-		});
-	
+        sendCommand("/roku/rewind",null,function() {
+            response.tellWithCard("Rewinding");
+        });
+    
     },
     Fastforward: function (intent, session, response) {
-		sendCommand("/roku/fastforward",null,function() {
-			response.tellWithCard("Fast forwarding");
-		});
-	
+        sendCommand("/roku/fastforward",null,function() {
+            response.tellWithCard("Fast forwarding");
+        });
+    
     },
     Instantreplay: function (intent, session, response) {
-		sendCommand("/roku/instantreplay",null,function() {
-			response.tellWithCard("Instant Replay");
-		});
-	
+        sendCommand("/roku/instantreplay",null,function() {
+            response.tellWithCard("Instant Replay");
+        });
+    
     },
     Up: function (intent, session, response) {
-		sendCommand("/roku/up",null,function() {
-			response.tellWithCard("Up");
-		});
-	
+        sendCommand("/roku/up",null,function() {
+            response.tellWithCard("Up");
+        });
+    
     },
     Uptwo: function (intent, session, response) {
-		sendCommand("/roku/uptwo",null,function() {
-			response.tellWithCard("Up Two");
-		});
-	
+        sendCommand("/roku/uptwo",null,function() {
+            response.tellWithCard("Up Two");
+        });
+    
     },
     Upthree: function (intent, session, response) {
-		sendCommand("/roku/upthree",null,function() {
-			response.tellWithCard("Up Three");
-		});
-	
+        sendCommand("/roku/upthree",null,function() {
+            response.tellWithCard("Up Three");
+        });
+    
     },
     Upfour: function (intent, session, response) {
-		sendCommand("/roku/upfour",null,function() {
-			response.tellWithCard("Up Four");
-		});
-	
+        sendCommand("/roku/upfour",null,function() {
+            response.tellWithCard("Up Four");
+        });
+    
     },
     Upfive: function (intent, session, response) {
-		sendCommand("/roku/upfive",null,function() {
-			response.tellWithCard("Up five");
-		});
-	
+        sendCommand("/roku/upfive",null,function() {
+            response.tellWithCard("Up five");
+        });
+    
     },
     Down: function (intent, session, response) {
-		sendCommand("/roku/down",null,function() {
-			response.tellWithCard("Down");
-		});
-	
+        sendCommand("/roku/down",null,function() {
+            response.tellWithCard("Down");
+        });
+    
     },
     Downtwo: function (intent, session, response) {
-		sendCommand("/roku/downtwo",null,function() {
-			response.tellWithCard("Down Two");
-		});
-	
+        sendCommand("/roku/downtwo",null,function() {
+            response.tellWithCard("Down Two");
+        });
+    
     },
     Downthree: function (intent, session, response) {
-		sendCommand("/roku/downthree",null,function() {
-			response.tellWithCard("Down Three");
-		});
-	
+        sendCommand("/roku/downthree",null,function() {
+            response.tellWithCard("Down Three");
+        });
+    
     },
     Downfour: function (intent, session, response) {
-		sendCommand("/roku/downfour",null,function() {
-			response.tellWithCard("Down Four");
-		});
-		
+        sendCommand("/roku/downfour",null,function() {
+            response.tellWithCard("Down Four");
+        });
+        
     },
     Downfive: function (intent, session, response) {
-		sendCommand("/roku/downfive",null,function() {
-			response.tellWithCard("Down Five");
-		});
-	
+        sendCommand("/roku/downfive",null,function() {
+            response.tellWithCard("Down Five");
+        });
+    
     },
     Power: function (intent, session, response) {
-		sendCommand("/roku/power",null,function() {
-			response.tellWithCard("Power");
-		});
-	
+        sendCommand("/roku/power",null,function() {
+            response.tellWithCard("Power");
+        });
+    
     },
     Left: function (intent, session, response) {
-		sendCommand("/roku/left",null,function() {
-			response.tellWithCard("Left");
-		});
-	
+        sendCommand("/roku/left",null,function() {
+            response.tellWithCard("Left");
+        });
+    
     }, 
     Lefttwo: function (intent, session, response) {
-		sendCommand("/roku/lefttwo",null,function() {
-			response.tellWithCard("Left Two");
-		});
-	
+        sendCommand("/roku/lefttwo",null,function() {
+            response.tellWithCard("Left Two");
+        });
+    
     },
     Leftthree: function (intent, session, response) {
-		sendCommand("/roku/leftthree",null,function() {
-			response.tellWithCard("Left Three");
-		});
-	
+        sendCommand("/roku/leftthree",null,function() {
+            response.tellWithCard("Left Three");
+        });
+    
     },
     Leftfour: function (intent, session, response) {
-		sendCommand("/roku/leftfour",null,function() {
-			response.tellWithCard("Left Four");
-		});
-	
+        sendCommand("/roku/leftfour",null,function() {
+            response.tellWithCard("Left Four");
+        });
+    
     },
     Leftfive: function (intent, session, response) {
-		sendCommand("/roku/leftfive",null,function() {
-			response.tellWithCard("Left Five");
-		});
-	
+        sendCommand("/roku/leftfive",null,function() {
+            response.tellWithCard("Left Five");
+        });
+    
     },
     Right: function (intent, session, response) {
-		sendCommand("/roku/right",null,function() {
-			response.tellWithCard("Right");
-		});
-	
+        sendCommand("/roku/right",null,function() {
+            response.tellWithCard("Right");
+        });
+    
     },   
     Righttwo: function (intent, session, response) {
-		sendCommand("/roku/righttwo",null,function() {
-			response.tellWithCard("Right Two");
-		});
-	
+        sendCommand("/roku/righttwo",null,function() {
+            response.tellWithCard("Right Two");
+        });
+    
     },
     Rightthree: function (intent, session, response) {
-		sendCommand("/roku/rightthree",null,function() {
-			response.tellWithCard("Right Three");
-		});
-	
+        sendCommand("/roku/rightthree",null,function() {
+            response.tellWithCard("Right Three");
+        });
+    
     },
     Rightfour: function (intent, session, response) {
-		sendCommand("/roku/rightfour",null,function() {
-			response.tellWithCard("Right Four");
-		});
-	
+        sendCommand("/roku/rightfour",null,function() {
+            response.tellWithCard("Right Four");
+        });
+    
     },
     Rightfive: function (intent, session, response) {
-		sendCommand("/roku/rightfive",null,function() {
-			response.tellWithCard("Right Five");
-		});
-	
+        sendCommand("/roku/rightfive",null,function() {
+            response.tellWithCard("Right Five");
+        });
+    
     },
     Type: function (intent, session, response) {
-		sendCommand("/roku/type",intent.slots.Text.value,function() {
-			response.tellWithCard("Typing text: "+intent.slots.Text.value,"Roku","Typing text: "+intent.slots.Text.value);
-		});
+        sendCommand("/roku/type",intent.slots.Text.value,function() {
+            response.tellWithCard("Typing text: "+intent.slots.Text.value,"Roku","Typing text: "+intent.slots.Text.value);
+        });
     },
-	PlayPause: function (intent, session, response) {
-		sendCommand("/roku/playpause",null,function() {
-			response.tell("Ok");
-		});
+    PlayPause: function (intent, session, response) {
+        sendCommand("/roku/playpause",null,function() {
+            response.tell("Ok");
+        });
     },
     Playlastyoutube: function (intent, session, response) {
-		sendCommand("/roku/playlastyoutube",null,function() {
-			response.tellWithCard("Playing Last Search on YouTube");
-		});
-	
+        sendCommand("/roku/playlastyoutube",null,function() {
+            response.tellWithCard("Playing Last Search on YouTube");
+        });
+    
     },
     SearchRoku: function (intent, session, response) {
-		sendCommand("/roku/searchroku",intent.slots.Text.value,function() {
-			response.tellWithCard("Playing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
-		});
+        sendCommand("/roku/searchroku",intent.slots.Text.value,function() {
+            response.tellWithCard("Playing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
+        });
     },
     Search: function (intent, session, response) {
-		sendCommand("/roku/search",intent.slots.Text.value,function() {
-			response.tellWithCard("Typing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
-		});
+        sendCommand("/roku/search",intent.slots.Text.value,function() {
+            response.tellWithCard("Typing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
+        });
     },
     SearchPlex: function (intent, session, response) {
-		sendCommand("/roku/searchplex",intent.slots.Text.value,function() {
-			response.tellWithCard("Playing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
-		});
+        sendCommand("/roku/searchplex",intent.slots.Text.value,function() {
+            response.tellWithCard("Playing: "+intent.slots.Text.value,"Roku","Playing: "+intent.slots.Text.value);
+        });
     },
     HelpIntent: function (intent, session, response) {
         response.tell("No help available at this time.");
